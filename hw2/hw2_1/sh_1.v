27d0b0f0100c1c72324995dd44e20a61add44587
Xuyi Ruan <xruan@wisc.edu>
2016-02-12 12:04:53 -0600

    added msb_mod.do

--- hw2/hw2_1/sh_1.v
+++ hw2/hw2_1/sh_1.v
@@ -13,7 +13,7 @@ module sh_1 (Out, In, Op, En);
     input En;
     output [15:0] Out;
    
-    msb_module bit15(.Out(Out[15]), .InA(0), .InB(In[15]), .InC(In[14]), .InD(In[15]), .En(En), .Op(Op));
+    msb_module bit15(.Out(Out[15]), .InA(1'b0), .InB(In[15]), .InC(In[14]), .InD(In[15]), .En(En), .Op(Op));
     inner_module bit14(.Out(Out[14]), .InA(In[15]), .InB(In[13]), .InC(In[14]), .En(En), .Op(Op[1]));
     inner_module bit13(.Out(Out[13]), .InA(In[14]), .InB(In[12]), .InC(In[13]), .En(En), .Op(Op[1]));
     inner_module bit12(.Out(Out[12]), .InA(In[13]), .InB(In[11]), .InC(In[12]), .En(En), .Op(Op[1]));
@@ -28,6 +28,6 @@ module sh_1 (Out, In, Op, En);
     inner_module bit3(.Out(Out[3]), .InA(In[4]), .InB(In[2]), .InC(In[3]), .En(En), .Op(Op[1]));
     inner_module bit2(.Out(Out[2]), .InA(In[3]), .InB(In[1]), .InC(In[2]), .En(En), .Op(Op[1]));
     inner_module bit1(.Out(Out[1]), .InA(In[2]), .InB(In[0]), .InC(In[1]), .En(En), .Op(Op[1]));
-    lsb_module bit0(.Out(Out[0]), .InA(0), .InB(In[15]), .InC(In[1]), .InD(In[0]), .En(En), .Op(Op));
+    lsb_module bit0(.Out(Out[0]), .InA(1'b0), .InB(In[15]), .InC(In[1]), .InD(In[0]), .En(En), .Op(Op));
 
 endmodule
--- hw2/hw2_1/sh_4.v
+++ hw2/hw2_1/sh_4.v
@@ -17,10 +17,10 @@ module sh_4 (Out, In, Op, En);
     // Op for inner_moduler has only 1-bit. ex. Op[1]
     // 
    
-    msb_module bit15(.Out(Out[15]), .InA(0), .InB(In[15]), .InC(In[11]), .InD(In[15]), .En(En), .Op(Op));
-    msb_module bit14(.Out(Out[14]), .InA(0), .InB(In[15]), .InC(In[10]), .InD(In[14]), .En(En), .Op(Op));
-    msb_module bit13(.Out(Out[13]), .InA(0), .InB(In[15]), .InC(In[9]), .InD(In[13]), .En(En), .Op(Op));
-    msb_module bit12(.Out(Out[12]), .InA(0), .InB(In[15]), .InC(In[8]), .InD(In[12]), .En(En), .Op(Op));
+    msb_module bit15(.Out(Out[15]), .InA(1'b0), .InB(In[15]), .InC(In[11]), .InD(In[15]), .En(En), .Op(Op));
+    msb_module bit14(.Out(Out[14]), .InA(1'b0), .InB(In[15]), .InC(In[10]), .InD(In[14]), .En(En), .Op(Op));
+    msb_module bit13(.Out(Out[13]), .InA(1'b0), .InB(In[15]), .InC(In[9]), .InD(In[13]), .En(En), .Op(Op));
+    msb_module bit12(.Out(Out[12]), .InA(1'b0), .InB(In[15]), .InC(In[8]), .InD(In[12]), .En(En), .Op(Op));
     inner_module bit11(.Out(Out[11]), .InA(In[15]), .InB(In[7]), .InC(In[11]), .En(En), .Op(Op[1]));
     inner_module bit10(.Out(Out[10]), .InA(In[14]), .InB(In[6]), .InC(In[10]), .En(En), .Op(Op[1]));
     inner_module bit9(.Out(Out[9]), .InA(In[13]), .InB(In[5]), .InC(In[9]), .En(En), .Op(Op[1]));
@@ -29,9 +29,9 @@ module sh_4 (Out, In, Op, En);
     inner_module bit6(.Out(Out[6]), .InA(In[10]), .InB(In[2]), .InC(In[6]), .En(En), .Op(Op[1]));
     inner_module bit5(.Out(Out[5]), .InA(In[9]), .InB(In[1]), .InC(In[5]), .En(En), .Op(Op[1]));
     inner_module bit4(.Out(Out[4]), .InA(In[8]), .InB(In[0]), .InC(In[4]), .En(En), .Op(Op[1]));
-    lsb_module bit3(.Out(Out[3]), .InA(0), .InB(In[15]), .InC(In[7]), .InD(In[3]), .En(En), .Op(Op));
-    lsb_module bit2(.Out(Out[2]), .InA(0), .InB(In[14]), .InC(In[6]), .InD(In[2]), .En(En), .Op(Op));
-    lsb_module bit1(.Out(Out[1]), .InA(0), .InB(In[13]), .InC(In[5]), .InD(In[1]), .En(En), .Op(Op));
-    lsb_module bit0(.Out(Out[0]), .InA(0), .InB(In[12]), .InC(In[4]), .InD(In[0]), .En(En), .Op(Op));
+    lsb_module bit3(.Out(Out[3]), .InA(1'b0), .InB(In[15]), .InC(In[7]), .InD(In[3]), .En(En), .Op(Op));
+    lsb_module bit2(.Out(Out[2]), .InA(1'b0), .InB(In[14]), .InC(In[6]), .InD(In[2]), .En(En), .Op(Op));
+    lsb_module bit1(.Out(Out[1]), .InA(1'b0), .InB(In[13]), .InC(In[5]), .InD(In[1]), .En(En), .Op(Op));
+    lsb_module bit0(.Out(Out[0]), .InA(1'b0), .InB(In[12]), .InC(In[4]), .InD(In[0]), .En(En), .Op(Op));
 
 endmodule
