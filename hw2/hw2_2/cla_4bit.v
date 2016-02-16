31bbcde94133c1c77956c312f28de317ce5c4c84
Xuyi Ruan <xruan@wisc.edu>
2016-02-15 22:31:56 -0600

    xxx

--- hw2/hw2_2/cla_4bit.v
+++ hw2/hw2_2/cla_4bit.v
@@ -1,7 +1,8 @@
-module cla_4bit(P, G, CI, CO);
-input [3:0] P, G;
-input CI;
-output [3:0] CO;
-output PG, GG;
+module cla_4bit (CO, PG, GG, CI, P, G);
+    input [3:0] P, G;
+    input CI;
+    output [3:0] CO;
+    output PG, GG;
+
 
 
