2b44f6e8ee2ae654607e86923c871241c5ec61b2
Yudong Sun <sun82@wisc.edu>
2016-03-15 03:55:01 -0500

    added writeback

---
+++ project/demo1/verilog/writeback.v
@@ -0,0 +1,30 @@
+module writeback(write_data, RegDataSrc, mem_data_out, alu_out, imm_8_ext, pc_plus_two, cond_set);
+
+  input [2:0] RegDataSrc;
+  input [15:0] mem_data_out;
+  input [15:0] alu_out;
+  input [15:0] imm_8_ext;
+  input [15:0] pc_plus_two
+  input [15:0] cond_set;
+  output [15:0] write_data;
+  
+
+  mux8_1_16bit alu_src_mux_2(
+          // Outputs
+          .out(write_data),
+          // Inputs
+          .sel(RegDataSrc),
+          .in0(mem_data_out),
+          .in1(alu_out),
+          .in2(imm_8_ext),
+          .in3(pc_plus_two),
+          .in4(cond_set),
+          .in5(16'bxxxx_xxxx_xxxx_xxxx),
+          .in6(16'bxxxx_xxxx_xxxx_xxxx),
+          .in7(16'bxxxx_xxxx_xxxx_xxxx)
+        );
+
+
+
+endmodule
+
