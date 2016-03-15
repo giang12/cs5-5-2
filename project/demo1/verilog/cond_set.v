f5615b6d639441f974ebbd832dfb98fcd9513bb5
Yudong Sun <sun82@wisc.edu>
2016-03-15 03:37:21 -0500

    added execution stage

--- project/demo1/verilog/cond_set.v
+++ project/demo1/verilog/cond_set.v
@@ -1,48 +1,48 @@
-module cond_set(set, instr, zero, Ofl, alu_out_msb);
+module cond_set(set, instr, zero, ofl, alu_out_msb);
     input zero, ofl, alu_out_msb;
     input [1:0] instr;
-    output set;
-    reg set;
+    output [15:0] set;
+    reg [15:0] set;
 
 always @ (zero, ofl, alu_out_msb, instr)
 begin
     casex({instr,zero,ofl,alu_out_mst})
       5'b000xx:
       begin
-        set <= 1'b0;
+        set <= 16'b0000_0000_0000_0000;
       end
       5'b001xx:
       begin
-        set <= 1'b1;
+        set <= 16'b0000_0000_0000_0001;
       end
       5'b01xx0:
       begin
-        set <= 1'b0;
+        set <= 16'b0000_0000_0000_0000;
       end
       5'b01xx1:
       begin
-        set <= 1'b1;
+        set <= 16'b0000_0000_0000_0001;
       end
       5'b10xx0:
       begin
-        set <= 1'b1;
+        set <= 16'b0000_0000_0000_0001;
       end
       5'b10xx1:
       begin
-        set <= 1'b0;
+        set <= 16'b0000_0000_0000_0000;
       end
       5'b11x0x:
       begin
-        set <= 1'b0;
+        set <= 16'b0000_0000_0000_0000;
       end
       5'b11x1x:
       begin
-        set <= 1'b1;
+        set <= 16'b0000_0000_0000_0001;
       end
       
       default:
       begin
-        set <= 1'bx;
+        set <= 16'bxxxx_xxxx_xxxx_xxxx;
       end
     endcase
 end   
---
+++ project/demo1/verilog/execution.v
@@ -0,0 +1,177 @@
+module execution(next_pc, Out, set, instr, pc_plus_two, pc, read_data_1, read_data_2, imm_5_ext, imm8_ext, imm_11_ext, ALUSrc1, ALUSrc2, Op, Cin, invA, invB, sign, jump, branch);
+ 
+  input [15:0] instr;
+  input [15:0] pc_plus_two;
+  input [15:0] pc;
+  input [15:0] read_data_1, read_data_2
+  input [15:0] imm_5_ext, imm_8_ext, imm_11_ext;
+  input [2:0] ALUSrc1, ALUSrc2;
+  input [2:0] Op;
+  input Cin, invA, invB, sign;
+  input jump;
+  input branch;
+
+  //NEED INPUT FOR 
+  // write back data [15:0]
+  
+  output [15:0] next_pc;
+  output [15:0] Out; 
+  output [15:0] set;
+  //NEED OUTPUT FOR 
+  // instr [15:0]
+  // imm_8_ext [15:0]
+  // read_data_2 [15:0]
+  // write back data [15:0]
+
+
+  wire [15:0] read_data_1_shifted;
+  wire [15:0] alu_out;
+  wire ofl;
+  wire zero;
+  wire [15:0] alu_src_1, alu_src_2;
+  wire [15:0] pc_plus_two_plus_imm_8_ext;
+  wire [15:0] pc_plus_two_plus_imm_11_ext;
+  wire [15:0] rs_plus_imm_8_ext;
+  wire [2:0] pc_src;
+  wire [15:0] branch_target;
+  wire ofl_disposal;
+
+  
+
+  assign Out = alu_out;
+
+  
+  // ALU Related 
+  sf_left8bit shifter_1(
+          // Outputs
+          .out(read_data_1_shifted),
+          // Inputs
+          .in(read_data_1)
+        );
+  
+  mux8_1_16bit alu_src_mux_1(
+          // Outputs
+          .out(alu_src_1),
+          // Inputs
+          .sel(ALUSrc1),
+          .in0(read_data_1),
+          .in1(read_data_1_shifted),
+          .in2(imm_5_ext),
+          .in3(imm_8_ext),
+          .in4(read_data_2),
+          .in5(16'b0000_0000_0000_0000),
+          .in6(16'bxxxx_xxxx_xxxx_xxxx),
+          .in7(16'bxxxx_xxxx_xxxx_xxxx)
+        );
+  mux8_1_16bit alu_src_mux_2(
+          // Outputs
+          .out(alu_src_2),
+          // Inputs
+          .sel(ALUSrc2),
+          .in0(read_data_2),
+          .in1(imm_5_ext),
+          .in2(imm_8_ext),
+          .in3(read_data_1),
+          .in4(16'b0000_0000_0000_0000),
+          .in5(16'bxxxx_xxxx_xxxx_xxxx),
+          .in6(16'bxxxx_xxxx_xxxx_xxxx),
+          .in7(16'bxxxx_xxxx_xxxx_xxxx)
+        );
+
+  alu alu0(
+          // Outputs
+          .Out(alu_out),
+          .Ofl(ofl),
+          .Z(zero),
+          // Inputs
+          .A(alu_src_1),
+          .B(alu_src_2),
+          .Cin(Cin),
+          .Op(Op),
+          .invA(invA),
+          .invB(invB),
+          .sign(sign)
+        );
+
+  // Two other sub control units
+  cond_set cond_set0(
+          // Outputs
+          .set(set),
+          // Inputs
+          .instr(instr[12:11]),
+          .zero(zero),
+          .ofl(ofl),
+          .alu_out_msb(alu_out[15])
+        );
+
+  pc_ctr pc_ctr0(
+          // Outputs
+          .pc_src(pc_src),
+          // Inputs
+          .instr(instr[12:11])
+          .jump(jump),
+          .zero(zero),
+          .alu_out_msb(alu_out[15])
+        );
+
+  // Branch Related Part
+
+  mux2_1_16bit next_pc_mux(
+          // Outputs
+          .out(next_pc),
+          // Inputs
+          .sel(branch),
+          .in0(pc_plus_two),
+          .in1(branch_target)
+        );
+
+  cla_16bit adder0(
+          // Outputs
+          .OUT(rs_plus_imm_8_ext),
+          .Ofl(ofl_disposal),
+          // Inputs
+          .A(read_data_1),
+          .B(imm_8_ext),
+          .CI(1'b0),
+          .sign(1'b1)
+        ); 
+
+  cla_16bit adder1(
+          // Outputs
+          .OUT(pc_plus_two_plus_imm_8_ext),
+          .Ofl(ofl_disposal),
+          // Inputs
+          .A(pc_plus_two),
+          .B(imm_8_ext),
+          .CI(1'b0),
+          .sign(1'b1)
+        ); 
+
+  cla_16bit adder2(
+          // Outputs
+          .OUT(pc_plus_two_plus_imm_11_ext),
+          .Ofl(ofl_disposal),
+          // Inputs
+          .A(pc_plus_two),
+          .B(imm_11_ext),
+          .CI(1'b0),
+          .sign(1'b1)
+        ); 
+
+  mux8_1_16bit branch_target_mux(
+          // Outputs
+          .out(branch_target),
+          // Inputs
+          .sel(pc_src),
+          .in0(pc),
+          .in1(pc_plus_two),
+          .in2(rs_plus_imm_8_ext),
+          .in3(pc_plus_two_plus_imm_8_ext),
+          .in4(pc_plus_two_plus_imm_11_ext),
+          .in5(16'bxxxx_xxxx_xxxx_xxxx),
+          .in6(16'bxxxx_xxxx_xxxx_xxxx),
+          .in7(16'bxxxx_xxxx_xxxx_xxxx)
+        );
+
+
+endmodule
