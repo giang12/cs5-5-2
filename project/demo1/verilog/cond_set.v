0fd667e7f254300fadc8b2d95aebbb72248003f7
Yudong Sun <sun82@wisc.edu>
2016-03-15 00:03:47 -0500

    pc_ctr and cond_set filled

--- project/demo1/verilog/cond_set.v
+++ project/demo1/verilog/cond_set.v
@@ -1,7 +1,44 @@
-module cond_set(cond, instr, zero, Ofl, out);
-    input zero, Ofl, out;
+module cond_set(set, instr, zero, Ofl, alu_out_msb);
+    input zero, ofl, alu_out_msb;
     input [1:0] instr;
-    output [15:0] cond;
+    output set;
+    reg set;
 
-
+always @ (zero, ofl, alu_out_msb, instr)
+begin
+    casex({instr,zero,ofl,alu_out_mst})
+      5'b000xx:
+      begin
+        set <= 1'b0;
+      end
+      5'b001xx:
+      begin
+        set <= 1'b1;
+      end
+      5'b01xx0:
+      begin
+        set <= 1'b0;
+      end
+      5'b01xx1:
+      begin
+        set <= 1'b1;
+      end
+      5'b10xx0:
+      begin
+        set <= 1'b1;
+      end
+      5'b10xx1:
+      begin
+        set <= 1'b0;
+      end
+     5'b11x0x:
+      begin
+        set <= 1'b0;
+      end
+     5'b11x1x:
+      begin
+        set <= 1'b1;
+      end
+    endcase
+end   
 endmodule
--- project/demo1/verilog/pc_ctr.v
+++ project/demo1/verilog/pc_ctr.v
@@ -1,6 +1,60 @@
-module pc_ctr(pc_src, jump, zero, alu_out, instr);
-    input jump, zero, alu_out;
+module pc_ctr(pc_src, jump, zero, alu_out_msb, instr);
+    input jump, zero, alu_out_msb;
     input [1:0] instr;
     output [2:0] pc_src;
+    reg [2:0] pc_src;
 
+always @ (jump, instr, zero, alu_out_msb)
+begin
+    casex({jump,instr,zero,alu_out_msb})
+      5'b0000x:
+      begin
+        pc_src <= 3'b001;
+      end
+      5'b0001x:
+      begin
+        pc_src <= 3'b011;
+      end
+      5'b0010x:
+      begin
+        pc_src <= 3'b011;
+      end
+      5'b0011x:
+      begin
+        pc_src <= 3'b001;
+      end
+      5'b010x0:
+      begin
+        pc_src <= 3'b001;
+      end
+      5'b010x1:
+      begin
+        pc_src <= 3'b011;
+      end
+      5'b011x0:
+      begin
+        pc_src <= 3'b011;
+      end
+      5'b011x1:
+      begin
+        pc_src <= 3'b001;
+      end
+      5'b100xx:
+      begin
+        pc_src <= 3'b100;
+      end
+      5'b101xx:
+      begin
+        pc_src <= 3'b010;
+      end
+      5'b110xx:
+      begin
+        pc_src <= 3'b100;
+      end
+      5'b111xx:
+      begin
+        pc_src <= 3'b010;
+      end
+    endcase
+end
 endmodule
