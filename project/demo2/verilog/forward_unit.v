1f3112171725df2b4075d95b73bf0dd496d4e797
Xuyi Ruan <xruan@wisc.edu>
2016-04-11 05:15:59 -0500

    updated forward_unit

--- project/demo2/verilog/forward_unit.v
+++ project/demo2/verilog/forward_unit.v
@@ -1,6 +1,18 @@
-module forward_unit(ALUSel1, ALUSel2, IDEX_Instr, EXMEM_Instr, MEMWB_Instr, EXMEM_wb, MEMWB_wb); 
+module forward_unit(ALUSel1, 
+                    ALUSel2, 
+                    IDEX_Instr, 
+                    EXMEM_Instr, 
+                    MEMWB_Instr, 
+                    EXMEM_RegWriteEN, 
+                    MEMWB_RegWriteEN, 
+                    IDEX_ALUSrc1, 
+                    IDEX_ALUSrc2, 
+                    EXMEM_RegDst, 
+                    MEMWB_RegDst); 
+    
     input [15:0] IDEX_Instr, EXMEM_Instr, MEMWB_Instr;
-    input EXMEM_wb, MEMWB_wb;
+    input EXMEM_RegWriteEN, MEMWB_RegWriteEN;
+    input IDEX_ALUSrc1, IDEX_ALUSrc2, EXMEM_RegDst, MEMWB_RegDst;
     output ALUSel1, ALUSel2;
     
     // your fancy code goes here
